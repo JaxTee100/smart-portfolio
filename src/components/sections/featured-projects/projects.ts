5ccfc9895259d9fc9d8be93b8018c82707aa9a56
Udeh Tochukwu Tobias <jaxj9449@gmail.com>
2024-11-12 16:31:51 +0100

    worked on eradicating all deployment errors

--- src/components/sections/featured-projects/projects.ts
+++ src/components/sections/featured-projects/projects.ts
@@ -1,13 +1,17 @@
 export const projects = [
     {
+        id: 'project1',
+        title: 'Thread Replica',
+
+
+    },
+    {
+        id: 'project2',
         title: 'Thread Replica',
 
     },
     {
-        title: 'Thread Replica',
-
-    },
-    {
+        id: 'project3',
         title: 'Thread Replica',
 
     },
--- src/components/sections/profile-section/ProfileSection.tsx
+++ src/components/sections/profile-section/ProfileSection.tsx
@@ -2,7 +2,6 @@
 import Image from "next/image";
 import Link from "next/link";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
-import Typical from 'react-typical';
 import { Typewriter } from 'react-simple-typewriter'
 
 const ProfileSection = () => {
